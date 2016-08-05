5901912f9527d2bb4d6dae4f73496e2de8f813c6
Fabien C. Y. Benureau <fabien.benureau+git@gmail.com>
2016-08-05 12:52:30 +0200

    Removed grid unit layout in layers
    This was cumbersome, and was purely esthetics at that point.

--- leabra/layer.py
+++ leabra/layer.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 from .unit import Unit
 
 
@@ -14,55 +12,51 @@ class LayerConstants(object):
 class Layer(object):
     """Leabra Layer class"""
 
-    def __init__(self, shape, params=None):
+    def __init__(self, size, params=None):
         """
-        shape:  dimension of the layer matrix (row, columns)
+        size:  number of units in the layer
         """
         self.cst = params
         if self.cst is None:
             self.cst = LayerConstants()
 
-        self.shape = shape
-        self.N = shape[0]*shape[1]
-        self.k = int((1-self.cst.kwta_pct)*len(self))
-        assert 0 < self.k < self.N
+        self.size = size
+        self.units = [Unit() for _ in range(self.size)]
 
-        self.units = [[Unit() for j in range(shape[1])]
-                              for i in range(shape[0])]
         self.g_i = 0.0
 
-    def __len__(self):
-        """Return the number of units in the layer"""
-        return self.shape[0]*self.shape[1]
+    @property
+    def k(self):
+        """Derived from cst.kwta_pct, the number of inhibited units"""
+        return int((1 - self.cst.kwta_pct) * self.size)
 
     @property
     def activities(self):
         """Return the matrix of the units's activities"""
-        return np.array([[self.units[i][j].act for j in range(self.shape[1])]
-                                               for i in range(self.shape[0]) ])
+        return [u.act for u in self.units]
 
     def set_activities(self, inputs):
         """Set the units's activities equal to the inputs"""
-        for (i, j), inp in np.ndenumerate(inputs):
-            self.units[i][j].set_activity(inp)
+        assert len(inputs) == self.size
+        for u, inp in zip(self.units, inputs):
+            u.set_activity(inp)
 
     def _active_threshold(self, u):
         """Threshold of kWTA inhibition. See eq. A7."""
-        g_e_star = u.g_e - u.cst.bias/self.N
+        g_e_star = u.g_e - u.cst.bias/self.size
         return (  u.cst.g_bar_e * g_e_star  * (u.cst.e_rev_e - u.cst.act_thr)  # eq. A7
                 + u.cst.g_bar_l * u.cst.g_l * (u.cst.e_rev_l - u.cst.act_thr)
                 )/ (u.cst.act_thr - u.cst.e_rev_i)
 
     def _inhibition(self):
         """Compute inhibition"""
-        g_thrs = [self._active_threshold(u) for _, u in np.ndenumerate(self.units)]
+        g_thrs = [self._active_threshold(u) for u in self.units]
         g_thrs.sort()
         return g_thrs[self.k] + self.cst.q * (g_thrs[self.k] - g_thrs[self.k-1])
 
     def cycle(self, inputs):
         """Update the state of the layer"""
+        assert len(inputs) == self.size
         self.g_i = self._inhibition()
-        inputs = np.array(inputs)
-        assert inputs.shape == self.shape
-        for (i, j), net_raw in np.ndenumerate(inputs):
-            self.units[i][j].cycle(net_raw, g_i=self.g_i)
+        for u, net_raw in zip(self.units, inputs):
+            u.cycle(net_raw, g_i=self.g_i)
