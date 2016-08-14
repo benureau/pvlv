kWTA average inhibition, fix k semantics and edge-cases + tests

leabra/connection.py
@@ -12,8 +12,8 @@ class Link:
 
 class ConnectionSpec:
 
-    legal_lrule = (None, 'delta', 'xcal') # available values for self.lrule
-    legal_proj  = ('Full', 'OneToOne')    #              ... for self.proj
+    legal_lrule = None, 'delta', 'xcal' # available values for self.lrule
+    legal_proj  = 'full', '1to1'        #              ... for self.proj
 
     def __init__(self, **kwargs):
         """Connnection parameters"""
@@ -21,8 +21,8 @@ class ConnectionSpec:
         self.force = False   # activity are set directly in the post_layer
         self.inhib = False   # if True, inhibitory connection
         self.w0    = 1.0     # intial weights
-        self.proj  = 'Full'  # connection pattern between units.
-                             # Can be 'Full' or 'OneToOne'. In the latter case,
+        self.proj  = 'full'  # connection pattern between units.
+                             # Can be 'Full' or '1to1'. In the latter case,
                              # the layers must have the same size.
         self.lrule = None    # the learning rule to use. Possible values are
                              # 'delta', 'xcal' and None.
@@ -51,7 +51,7 @@ class Connection:
         # creating unit-to-unit links
         self.links = []
         assert self.spec.proj in self.spec.legal_proj
-        if self.spec.proj == 'OneToOne':
+        if self.spec.proj == '1to1':
             assert self.pre.size == self.post.size
             for pre_u, post_u in zip(self.pre.units, self.post.units):
                 self.links.append(Link(pre_u, post_u, self.spec.w0))
@@ -64,7 +64,7 @@ class Connection:
     @property
     def weights(self):
         """Return a matrix of the links weights"""
-        if self.spec.proj == 'OneToOne':
+        if self.spec.proj == '1to1':
             return np.array([[link.w for link in self.links]])
         else:
             W = np.zeros((len(self.pre.units), len(self.post.units)))  # weight matrix

leabra/layer.py
@@ -4,9 +4,14 @@ from .unit import Unit
 class LayerSpec:
     """Layer parameters"""
 
+    legal_inhib = 'kwta', 'kwta_avg'  # available values for self.inhib
+
     def __init__(self, **kwargs):
-        self.kwta_pct = 0.25 # proportion of active units
-        self.q = 0.25  # see eq. A6
+        self.inhib    = 'kwta'   # inhibition rule: 'kwta' or 'kwta_avg'
+        self.k        = None     # number of active units.
+        self.kwta_pct = 0.25     # proportion of active units; used to compute k
+                                 # only if self.k is None.
+        self.q        = 0.25     # see eq. A6
 
         for key, value in kwargs.items():
             setattr(self, key, value)
@@ -35,7 +40,9 @@ class Layer:
     @property
     def k(self):
         """Derived from cst.kwta_pct, the number of inhibited units"""
-        return int((1 - self.spec.kwta_pct) * self.size)
+        if self.spec.k is None:
+            return int(self.spec.kwta_pct * self.size)
+        else: return self.spec.k
 
     @property
     def activities(self):
@@ -57,11 +64,19 @@ class Layer:
 
     def _inhibition(self):
         """Compute inhibition"""
-        if self.k == 0:
-            return 0.0
         g_thrs = [self._active_threshold(u) for u in self.units]
-        g_thrs.sort()
-        return g_thrs[self.k] + self.spec.q * (g_thrs[self.k] - g_thrs[self.k-1])
+        g_thrs.sort(reverse=True)
+        if self.k >= self.size:  # no inhibition
+            return g_thrs[-1] - 0.01  # HACKISH
+        if self.k == 0:          # every unit is inhibited
+            return g_thrs[0]
+
+        # non-extreme cases
+        if self.spec.inhib.lower() == 'kwta':
+            return g_thrs[self.k] + self.spec.q * (g_thrs[self.k-1] - g_thrs[self.k])  # eq. A6
+        else:  # self.spec.inhib == 'avg_kwta'
+            top_avg, bottom_avg = np.mean(g_thrs[:k]), np.mean(g_thrs[k+1:])
+            return top_avg + self.spec.q * (top_avg - bottom_avg)  # eq. 3.6 [CECN1]
 
     def add_excitatory(self, inputs, forced_act=False):
         assert len(inputs) == self.size

tests/layer_test.py
@@ -2,21 +2,33 @@ import os
 import unittest
 import random
 
-import dotdot
-from leabra import Layer, LayerSpec
+import numpy as np
 
+import dotdot
+import leabra
 
 
 class LayerTests(unittest.TestCase):
     """Diverse checks on Layer behavior."""
 
     def test_set_activities(self):
-        layer = Layer(4)
+        layer = leabra.Layer(4)
         acts = [0.31, 0.63, -1.0, 1.0]
         layer.set_activities(acts)
 
         self.assertEqual(layer.activities, acts)
 
+    def test_inhibition(self):
+        inputs = np.linspace(0.5, 1.1, num=10)
+
+        for k in range(11):
+            layer_spec = leabra.LayerSpec(k=k)
+            layer = leabra.Layer(10, spec=layer_spec)
+            for i in range(100):
+                layer.add_excitatory(inputs)
+                layer.cycle()
+
+            self.assertEqual(len(np.nonzero(layer.activities)[0]), k)
 
 if __name__ == '__main__':
     unittest.main()